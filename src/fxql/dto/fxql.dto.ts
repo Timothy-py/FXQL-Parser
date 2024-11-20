d24f77836d3e5e29d78ba9945fb8a656b6648565
Timothy.py <adeyeyetimothy33@gmail.com>
2024-11-20 20:04:39 +0100

    ft<Documentation>:added OpenAPI documentation

--- src/fxql/dto/fxql.dto.ts
+++ src/fxql/dto/fxql.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateFxqlDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   FXQL: string;
