a90195b1e35d43fd346f48bb5af17ac8815a71f7
Timothy.py <adeyeyetimothy33@gmail.com>
2024-11-20 21:30:59 +0100

    ft<RateLimit>:implemented basic rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { DatabaseModule } from './database/database.module';
 import { ParserModule } from './parser/parser.module';
 import { FxqlModule } from './fxql/fxql.module';
 import { HelperService } from './helper/helper.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule, ParserModule, FxqlModule],
+  imports: [
+    DatabaseModule,
+    ParserModule,
+    FxqlModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 2,
+      },
+    ]),
+  ],
   controllers: [],
-  providers: [HelperService, Logger],
+  providers: [
+    HelperService,
+    Logger,
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+  ],
 })
 export class AppModule {}
